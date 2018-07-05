30c66fe6c8df2a65a055bdc77338d3d08a17ee86
TeodorPeev <t.peev1@gmail.com>
2018-07-05 13:16:30 +0300

    Fixed misunderstading of problem

--- demo-day-one/Example One/Program.cs
+++ demo-day-one/Example One/Program.cs
@@ -1,35 +1,28 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Numerics;
 namespace Example_One
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
             int n = int.Parse(Console.ReadLine());
-            double coef = 1;
-            int sum = 0;
-            int[,] matrix = new int[n, n];
+            double sum = 0;
+            double[,] matrix = new double[n, n];
             for(int i = 0; i<n; i++)
             {
-                for(int f = 0; f<n; f++)
+                for (int f = 0; f < n; f++)
                 {
-                    matrix[i, f] = (int)Math.Pow(2,coef);
-                    coef++;
-                    Console.Write(matrix[i, f] + " ");
-                    if(f >= i)
+                    matrix[i, f] = Math.Pow(2, i + f);
+                    if(i >= f)
                     {
                         sum += matrix[i, f];
                     }
                 }
-                Console.WriteLine();
             }
-            Console.WriteLine("Sum is {0}", sum);
+            BigInteger result = (BigInteger)sum; 
+            Console.WriteLine(result);
         }
     }
 }
