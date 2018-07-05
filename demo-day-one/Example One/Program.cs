6c49fe1f94e2298bdc9397c7200bfee469ee391b
TeodorPeev <t.peev1@gmail.com>
2018-07-05 11:24:17 +0300

    Matrix with power of 2

--- demo-day-one/Example One/Program.cs
+++ demo-day-one/Example One/Program.cs
@@ -11,7 +11,25 @@ namespace Example_One
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int n = int.Parse(Console.ReadLine());
+            double coef = 1;
+            int sum = 0;
+            int[,] matrix = new int[n, n];
+            for(int i = 0; i<n; i++)
+            {
+                for(int f = 0; f<n; f++)
+                {
+                    matrix[i, f] = (int)Math.Pow(2,coef);
+                    coef++;
+                    Console.Write(matrix[i, f] + " ");
+                    if(f >= n/2 && i >= n / 2)
+                    {
+                        sum += matrix[i, f];
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Sum is {0}", sum);
         }
     }
 }
